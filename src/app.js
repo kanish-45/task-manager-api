8c2d6dffc34a4e739a66e4129e697ecd51fd1b80
kanish-45 <kanishkaarthick.vm2023eee@sece.ac.in>
2026-03-07 19:31:44 +0530

    Added Kubernetes deployment, service, configmap and docker improvements

--- src/app.js
+++ src/app.js
@@ -3,17 +3,21 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Routes
 app.use("/tasks", taskRoutes);
 
+// Health Check Endpoint (for Kubernetes readiness/liveness later)
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
-const PORT = 5000;
+// Use environment variable for PORT (production best practice)
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// IMPORTANT: Bind to 0.0.0.0 for Docker/Kubernetes
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
